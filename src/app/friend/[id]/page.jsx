66e47f51b11c488b36077778a67e508b6bf55953
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2026-04-16 12:01:34 +0600

    bulid error fixed

--- src/app/friend/[id]/page.jsx
+++ src/app/friend/[id]/page.jsx
@@ -1,21 +1,19 @@
-
 import QuickCheckIn from "@/components/quickcheckin/QucikCheckIn";
-import { Questrial } from "next/font/google";
 import Image from "next/image";
-import {
-  FaClock,
-  FaArchive,
-  FaTrash,
-  
-} from "react-icons/fa";
+import { FaClock, FaArchive, FaTrash } from "react-icons/fa";
+import { promises as fs } from "fs"; 
+import path from "path"; 
 
 
 async function getFriendData(id) {
   try {
+    
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const res = await fetch(`http://localhost:3000/friendsData.json`);
-    if (!res.ok) return null;
-    const allFriends = await res.json();
+  
+    const filePath = path.join(process.cwd(), "public", "friendsData.json");
+    const jsonData = await fs.readFile(filePath, "utf8");
+    const allFriends = JSON.parse(jsonData);
 
     return allFriends.find((f) => String(f.id) === String(id));
   } catch (error) {
@@ -25,7 +23,6 @@ async function getFriendData(id) {
 }
 
 const FriendDetails = async ({ params }) => {
-
   const { id } = await params;
   const friend = await getFriendData(id);
 
@@ -33,7 +30,7 @@ const FriendDetails = async ({ params }) => {
     return (
       <div className="min-h-screen flex items-center justify-center">
         <h2 className="text-xl font-semibold text-gray-500 text-center">
-          Friend not found! <br />{" "}
+          Friend not found! <br />
           <span className="text-sm font-normal">
             Check if ID {id} exists in JSON.
           </span>
@@ -41,7 +38,6 @@ const FriendDetails = async ({ params }) => {
       </div>
     );
   }
-
 
   const statusKey = friend.status?.toLowerCase();
   const statusStyles = {
@@ -54,6 +50,7 @@ const FriendDetails = async ({ params }) => {
   return (
     <div className="bg-[#F8FAFC] min-h-screen p-4 md:p-10 font-sans">
       <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-12 gap-8">
+        {/* Left Column: Profile Card */}
         <div className="lg:col-span-4 flex flex-col gap-4">
           <div className="bg-white border border-[#E9E9E9] p-8 flex flex-col items-center rounded-sm shadow-sm">
             <div className="w-24 h-24 relative mb-4">
@@ -68,13 +65,11 @@ const FriendDetails = async ({ params }) => {
             <h2 className="text-xl font-semibold text-[#1F2937]">
               {friend.name}
             </h2>
-
             <span
               className={`mt-2 py-1.5 px-4 rounded-full text-xs font-medium text-white capitalize tracking-widest ${statusStyles[statusKey] || "bg-gray-400"}`}
             >
               {friend.status}
             </span>
-
             <div className="flex flex-wrap justify-center items-center gap-2 mt-4 mb-6">
               {friend.tags?.map((tag, index) => (
                 <span
@@ -85,7 +80,6 @@ const FriendDetails = async ({ params }) => {
                 </span>
               ))}
             </div>
-
             <p className="text-[#64748B] italic text-base font-medium text-center line-clamp-2">
               {friend.bio}
             </p>
@@ -107,40 +101,34 @@ const FriendDetails = async ({ params }) => {
           </div>
         </div>
 
+    
         <div className="lg:col-span-8 space-y-6">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <StatBox
               label="Days Since Contact"
-              value={ friend.days_since_contact}
+              value={friend.days_since_contact}
             />
-            <StatBox
-              label="Goal (Days)"
-              value={ friend.goal}
-            />
-            <StatBox
-              label="Next Due Date"
-              value={ friend.next_due_date}
-             
-            />
+            <StatBox label="Goal (Days)" value={friend.goal} />
+            <StatBox label="Next Due Date" value={friend.next_due_date} />
           </div>
 
           <div className="bg-white p-6 border border-[#E9E9E9] rounded-sm flex justify-between items-center shadow-sm">
             <div>
-              <h3 className="text-lg font-medium text-[#244D3F] capitalize  mb-1">
+              <h3 className="text-lg font-medium text-[#244D3F] capitalize mb-1">
                 Relationship Goal
               </h3>
               <p className="text-[#64748B] text-lg ">
                 Connect every{" "}
                 <span className="font-bold text-[#1F2937]">
-                  {friend.stats?.goal || friend.goal} days
+                  {friend.goal} days
                 </span>
               </p>
             </div>
-            <button className="py-2 px-4.5  bg-[#F8FAfC] border-on font-medium text-sm text-[#1F2937] rounded">
+            <button className="py-2 px-4.5 bg-[#F8FAfC] border-on font-medium text-sm text-[#1F2937] rounded">
               Edit
             </button>
           </div>
-          <QuickCheckIn friendName={friend.name}/>
+          <QuickCheckIn friendName={friend.name} />
         </div>
       </div>
     </div>
@@ -148,25 +136,20 @@ const FriendDetails = async ({ params }) => {
 };
 
 
-
 function ActionButton({ icon, label, isDanger = false }) {
   return (
     <button
-      className={`flex items-center justify-center gap-2 py-3 border border-[#E9E9E9] bg-white text-sm font-semibold rounded-sm hover:bg-opacity-90 transition-all shadow-sm ${
-        isDanger
-          ? "text-[#EF4444] hover:bg-red-50"
-          : "text-[#1F2937] hover:bg-gray-50"
-      }`}
+      className={`flex items-center justify-center gap-2 py-3 border border-[#E9E9E9] bg-white text-sm font-semibold rounded-sm hover:bg-opacity-90 transition-all shadow-sm ${isDanger ? "text-[#EF4444] hover:bg-red-50" : "text-[#1F2937] hover:bg-gray-50"}`}
     >
       {icon} {label}
     </button>
   );
 }
 
-function StatBox({ label, value}) {
+function StatBox({ label, value }) {
   return (
-    <div className="bg-white p-8 border  border-[#E9E9E9] rounded-sm flex flex-col items-center shadow-sm">
-      <span className="text-3xl tracking-tight font-semibold  text-[#244D3F]">
+    <div className="bg-white p-8 border border-[#E9E9E9] rounded-sm flex flex-col items-center shadow-sm">
+      <span className="text-3xl tracking-tight font-semibold text-[#244D3F]">
         {value}
       </span>
       <span className="mt-3 text-lg font-normal text-[#64748B] capitalize text-center">
@@ -175,7 +158,5 @@ function StatBox({ label, value}) {
     </div>
   );
 }
-
-
 
 export default FriendDetails;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,34 @@
 import Image from "next/image";
-
 import FriendCard from "@/components/FriendCard/FriendCard";
 import Banner from "@/components/home/Banner";
 import SummaryCard from "@/components/home/SummaryCard";
+import { promises as fs } from "fs";
+import path from "path";
 
+const friendData = async () => {
+ 
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  const filePath = path.join(process.cwd(), "public", "friendsData.json");
+  const jsonData = await fs.readFile(filePath, "utf8");
+  return JSON.parse(jsonData);
+};
 
-const friendData = async() => {
-    const FrienRes = await fetch("http://localhost:3000/friendsData.json");
-    return FrienRes.json()
-}
-
- const Home= async() =>{
-    const friendsData = await friendData()
+const Home = async () => {
+  const friendsData = await friendData();
 
   return (
     <div className="flex flex-col flex-1 bg-white font-sans">
-    
       <main className="flex flex-col w-full">
-        
-    <Banner />
-    <SummaryCard/>
-    <div className="grid max-w-7xl mb-5 md:mb-20  w-full mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {friendsData.map((friend) => (
-              <FriendCard key={friend.id} friend={friend} />
-              
-            ))}
-    </div>
+        <Banner />
+        <SummaryCard />
+        <div className="grid max-w-7xl mb-5 md:mb-20 w-full mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {friendsData.map((friend) => (
+            <FriendCard key={friend.id} friend={friend} />
+          ))}
+        </div>
       </main>
     </div>
   );
-}
-export default Home
+};
+
+export default Home;
