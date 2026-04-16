ae45d71add8d63db75188f754abc4aec7a0e911b
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2026-04-16 12:48:49 +0600

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
-    reactCompiler: true,
     images: {
         remotePatterns: [{
             protocol: "https",
             hostname: "randomuser.me",
-            pathname: "/**"
+            port: "",
+            pathname: "/api/portraits/**"
         }]
     }
-
-
-
-
 };
 
 export default nextConfig;
